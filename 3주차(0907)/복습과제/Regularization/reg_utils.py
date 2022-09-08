02acdc5ded4c4a0eef9380d4b73bf95e04d61d4f
bitamin <bitamin1818@gmail.com>
2022-09-09 00:14:13 +0900

    Revert "Revert "3주차 복습과제 / Regularization (#28)""
    This reverts commit 5865b60742273993a79173cb1c44f896ac773291.

--- 3주차(0907)/복습과제/Regularization/reg_utils.py
+++ 3주차(0907)/복습과제/Regularization/reg_utils.py
@@ -324,13 +324,12 @@ def plot_decision_boundary(model, X, y):
     plt.scatter(X[0, :], X[1, :], c=y, cmap=plt.cm.Spectral)
     plt.show()
     
-def load_2D_dataset():
-    data = scipy.io.loadmat('datasets/data.mat')
-    train_X = data['X'].T
-    train_Y = data['y'].T
-    test_X = data['Xval'].T
-    test_Y = data['yval'].T
+def load_2D_dataset():   
+    train_X = np.load('train_X.npy')
+    train_Y = np.load('train_Y.npy')
+    test_X = np.load('test_X.npy')
+    test_Y = np.load('test_Y.npy')
 
-    plt.scatter(train_X[0, :], train_X[1, :], c=train_Y, s=40, cmap=plt.cm.Spectral);
+    plt.scatter(train_X[0, :], train_X[1, :], c=train_Y[0], s=40, cmap=plt.cm.Spectral);
     
-    return train_X, train_Y, test_X, test_Y
+    return train_X, train_Y, test_X, test_Y 
